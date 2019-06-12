e464e0e4571d90442c6ffa45b6a73b0d2b32941e
akshay anand <akshayanand78@gmail.com>
2019-06-12 16:37:26 +0530

    dynamic hero pages done || redirected to heroes page if user tries to enter hero from url

--- src/components/heroes/heroDetails.jsx
+++ src/components/heroes/heroDetails.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import * as appConstants from '../../store/constants/appConstant';
+import {Redirect} from 'react-router-dom';
 
 class HeroDetails extends Component {
     constructor(props) {
@@ -9,31 +10,37 @@ class HeroDetails extends Component {
 
     render() {
         console.log(this.props);
+        const heroData = this.props.location.state;
         return (
-            <div className="popHero__wrapper">
-                <div className="popHero__imgContainer">
-                    <img src={appConstants.apiBase + this.props.location.state.heroData.img}
-                         alt={this.props.location.state.heroData.localized_name}/>
-                </div>
+            <div>
+                {
+                    heroData ?
+                        <div className="popHero__wrapper">
+                            <div className="popHero__imgContainer">
+                                <img src={appConstants.apiBase + this.props.location.state.heroData.img}
+                                     alt={this.props.location.state.heroData.localized_name}/>
+                            </div>
 
-                <div className="popHero__type">
-                    Type:- {this.props.location.state.heroData.attack_type}
-                </div>
+                            <div className="popHero__type">
+                                Type:- {this.props.location.state.heroData.attack_type}
+                            </div>
 
-                <div className="popHero__roles">
-                    Roles :- {this.props.location.state.heroData.roles.map((roles, index) => {
+                            <div className="popHero__roles">
+                                Roles :- {this.props.location.state.heroData.roles.map((roles, index) => {
 
-                    if (this.props.location.state.heroData.roles.length === index + 1) {
-                        return roles;
-                    } else {
-                        return roles + ",";
-                    }
+                                if (this.props.location.state.heroData.roles.length === index + 1) {
+                                    return roles;
+                                } else {
+                                    return roles + ",";
+                                }
 
-                })}
-                </div>
+                            })}
+                            </div>
 
+                        </div>
+                        : <Redirect to="/heroes"/>
+                }
             </div>
-
         )
 
     }
