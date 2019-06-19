927595a67c4a8cb9f766d17baae593f2759a6e01
akshay anand <akshayanand78@gmail.com>
2019-06-19 17:57:41 +0530

    mobile keyboard

--- src/components/heroes/heroes.jsx
+++ src/components/heroes/heroes.jsx
@@ -23,12 +23,12 @@ class Heroes extends Component {
         };
 
         this.searchString = '';
-        this.timeout = null;
+        // this.timeout = null;
     }
 
     //search function
     search = (e) => {
-        clearTimeout(this.timeout);
+        // clearTimeout(this.timeout);
         if ((e.keyCode >= 65 && e.keyCode <= 90) || e.keyCode === 32) {
             this.searchString = this.searchString + e.key;
             this.searchString && console.log(this.searchString);
@@ -48,12 +48,12 @@ class Heroes extends Component {
             }
 
         }
-        this.timeout = setTimeout( () => {
-            this.searchString = '';
-            this.setState({
-                heroStats: this.state.heroStats.map(hero => this.byNameFilter(hero))
-            })
-        }, 800)
+        // this.timeout = setTimeout(() => {
+        //     this.searchString = '';
+        //     this.setState({
+        //         heroStats: this.state.heroStats.map(hero => this.byNameFilter(hero))
+        //     })
+        // }, 800)
 
     }
 
@@ -150,7 +150,8 @@ class Heroes extends Component {
 
                         <div className="search__mobile">
                             <input type="checkbox" id="search"/>
-                            <input type='text' className="searchBar"/>
+                            <input type='text' onChange={(e) => this.searchString = e.target.value}
+                                   className="searchBar"/>
                             <label className="labelSearch" htmlFor="search">
                                 <i className="fa fa-search" aria-hidden="true"/>
                             </label>
